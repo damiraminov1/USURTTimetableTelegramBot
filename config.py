b0b85225633e41bf3358b78a6b7b597f88ba3aae
hecronk <damiraminov1@yandex.ru>
2022-08-25 17:54:44 +0500

    changed config, added requirements.txt

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__name__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class TelegramBotConfig(object):
-    TOKEN = 'PASTE HERE BOT FATHER TOKEN'
+    TOKEN = os.environ.get('BOT_TOKEN')
 
 
 class ParserConfig(object):
